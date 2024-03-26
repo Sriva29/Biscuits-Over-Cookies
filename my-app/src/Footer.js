0eadd97e75423ade2bbcd9971c87722f997d33e7
Sriva29 <63369374+Sriva29@users.noreply.github.com>
2024-03-26 13:49:58 -0400

    Footer completed

--- my-app/src/Footer.js
+++ my-app/src/Footer.js
@@ -1,13 +1,48 @@
-// import React from 'react';
-
 function Footer() {
   return (
-    <footer className="bg-light text-center text-lg-start">
+    <footer className="text-center text-lg-start text-white" style={{ backgroundColor: "#008080" }}>
+      <div className="container p-4">
+        <div className="row">
+          {/* Site Name & Tagline */}
+          <div className="col-lg-6 col-md-12 mb-4 mb-md-0">
+            <h5>TradeInsight</h5>
+            <p>
+              Generate a winning business plan and unlock the potential of international trade.
+            </p>
+          </div>
+
+          {/* Social Media & Contact */}
+          <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
+            <h5>Follow Us</h5>
+
+            <a className="btn btn-outline-light btn-floating m-1" href="#!" role="button">
+              <i className="fab fa-facebook-f"></i>
+            </a>
+            <a className="btn btn-outline-light btn-floating m-1" href="#!" role="button">
+              <i className="fab fa-twitter"></i>
+            </a>
+            <a className="btn btn-outline-light btn-floating m-1" href="#!" role="button">
+              <i className="fab fa-instagram"></i>
+            </a>
+          </div>
+
+          {/* Contact Details */}
+          <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
+            <h5>Contact Us</h5>
+            <p>
+              Phone: +123456789<br />
+              Email: info@tradeinsight.com
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {/* Links & Copyright */}
       <div className="text-center p-3" style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
-        © {new Date().getFullYear()} My Website:
-        <a className="text-dark" href="https://mywebsite.com/">
-          mywebsite.com
-        </a>
+        {/* <a className="text-white me-4" href="#!">Terms and Conditions</a>
+        <a className="text-white me-4" href="#!">Privacy Policy</a> */}
+        {/* <br /> */}
+        © {new Date().getFullYear()} TradeInsight: All rights reserved.
       </div>
     </footer>
   );
