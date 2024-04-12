6ac6c076e75b9648d8e9e9b5ed106d9c05ccfee0
Sriva29 <63369374+Sriva29@users.noreply.github.com>
2024-04-12 16:30:32 -0400

    updated head

--- my-app/src/Head.js
+++ my-app/src/Head.js
@@ -8,12 +8,12 @@ function Head() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="/">Tradei</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            {/* <Nav.Link href="/">Home</Nav.Link> */}
+            {/* <Nav.Link href="#link">Link</Nav.Link> */}
             <Nav.Link as={Link} to="/login">Login</Nav.Link>
             <Nav.Link as={Link} to="/signup">Sign Up</Nav.Link>
             <NavDropdown title="Profile" id="basic-nav-dropdown">
